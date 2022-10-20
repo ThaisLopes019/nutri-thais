d1d075dddec3686ee1f06433f1a82efb47ee0330
Rodrigo <rodrigo_pbro@hotmail.com>
2022-10-19 23:21:22 -0300

    :sparkles:feat: Adicionando componente para o Bloco3

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Bloco1 } from '../../components/Bloco1'
+import { Bloco2 } from '../../components/Bloco2'
+import { Bloco3 } from '../../components/Bloco3'
 
 export function Home() {
   return (
     <>
-      <Bloco1 />
+      <Bloco3 />
     </>
   )
 }
